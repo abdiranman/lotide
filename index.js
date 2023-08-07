db615ecd3264d4c392927705cdc5fc2948a6f924
abdiranman <abdirahman.abdi330@.com>
2023-08-06 21:00:21 -0400

    Exporting All Functions II

---
+++ index.js
@@ -0,0 +1,36 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPoisitions = require('./letterPoisitions');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual')
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  countLetters,
+  countOnly,
+  eqObjects,
+  map,
+  takeUntil,
+  without,
+  findKey,
+  findKeyByValue, 
+  letterPoisitions,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual
+};
