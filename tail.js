c1a05313623e0ecd6ff05846f645047ccbde6f04
abdiranman <abdirahman.abdi330@.com>
2023-08-06 20:31:28 -0400

    Refactored Tail function with test

--- tail.js
+++ tail.js
@@ -8,7 +8,5 @@ const assertEqual = require('./assertEqual');
   };
   
   
-  const words = ["Yo Yo", "Lighthouse", "Labs"];
-  const originalLength = words.length;
-  tail(words);
-  assertEqual(words.length, originalLength); 
+
+  module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,12 @@
 const tail = require('../tail')
+const assertEqual = require('../assertEqual')
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const originalLength = words.length;
+tail(words);
+assertEqual(words.length, originalLength); 
+
+
 
 module.exports = tail;
