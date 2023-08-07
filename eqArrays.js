6feea31ab802ea80a56d642623604022598494c8
abdiranman <abdirahman.abdi330@.com>
2023-08-06 20:33:28 -0400

    EqArrays refactored and tested

--- eqArrays.js
+++ eqArrays.js
@@ -14,8 +14,3 @@
 
   module.exports = eqArrays;
   
-  // Test Cases
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,3 +1,12 @@
 const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual')
+
+
+  // Test Cases
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
 
 module.exports = eqArrays;
