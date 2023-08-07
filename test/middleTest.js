5114a00e1ec621ad27e768fcd884fc0a52cf4e38
abdiranman <abdirahman.abdi330@.com>
2023-08-06 20:54:07 -0400

    Refactor with Mocha and Chai I

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,46 @@
+// const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle')
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 
-// TEST CODE
-assertArraysEqual(middle([1]), []); // Expected: []
-assertArraysEqual(middle([1, 2]), []); // Expected: []
-assertArraysEqual(middle([1, 2, 3]), [2]); // Expected: [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Expected: [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Expected: [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Expected: [3, 4]
+describe('middle', () => {
+    it('should return an empty array for input [1]', () => {
+      const result = middle([1]);
+      assert.deepEqual(result, []);
+    });
+  
+    it('should return an empty array for input [1, 2]', () => {
+      const result = middle([1, 2]);
+      assert.deepEqual(result, []);
+    });
+  
+    it('should return [2] for input [1, 2, 3]', () => {
+      const result = middle([1, 2, 3]);
+      assert.deepEqual(result, [2]);
+    });
+  
+    it('should return [3] for input [1, 2, 3, 4, 5]', () => {
+      const result = middle([1, 2, 3, 4, 5]);
+      assert.deepEqual(result, [3]);
+    });
+  
+    it('should return [2, 3] for input [1, 2, 3, 4]', () => {
+      const result = middle([1, 2, 3, 4]);
+      assert.deepEqual(result, [2, 3]);
+    });
+  
+    it('should return [3, 4] for input [1, 2, 3, 4, 5, 6]', () => {
+      const result = middle([1, 2, 3, 4, 5, 6]);
+      assert.deepEqual(result, [3, 4]);
+    });
+  });
+
+
+// // TEST CODE
+// assertArraysEqual(middle([1]), []); // Expected: []
+// assertArraysEqual(middle([1, 2]), []); // Expected: []
+// assertArraysEqual(middle([1, 2, 3]), [2]); // Expected: [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Expected: [3]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Expected: [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Expected: [3, 4]
 
 module.exports = middle;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,22 @@
+// const assertEqual = require('../assertEqual')
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const originalLength = words.length;
-tail(words);
-assertEqual(words.length, originalLength); 
+
+describe('tail', () => {
+    it('should not modify the original array length when called', () => {
+      const words = ["Yo Yo", "Lighthouse", "Labs"];
+      const originalLength = words.length;
+      tail(words);
+      assert.strictEqual(words.length, originalLength);
+    });
+  });
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const originalLength = words.length;
+// tail(words);
+// assertEqual(words.length, originalLength); 
 
 
 
